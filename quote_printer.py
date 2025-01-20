1aea6a682565ed91166ab5935f022589cab7d12e
CXLEB-99 <calebgoforth99@gmail.com>
2025-01-19 23:31:52 -0500

    Week-2 Commit Ex2.3

---
+++ quote_printer.py
@@ -0,0 +1,9 @@
+"""
+Author: Caleb Goforth
+Date: January 19, 2025
+File Name: quote_printer.py
+Description: This program prints a motivational quote and gives credit to the original author.
+"""
+
+# Printing a motivational quote along with the author's name.
+print("The only way to do great work is to love what you do. - Steve Jobs")
